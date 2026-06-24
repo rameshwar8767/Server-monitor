8c21207b8106f61ede1653354532afa8b648eb87
Rameshwar8767 <manerameshwar909@gmail.com>
2026-06-24 09:57:45 +0000

    completed

--- monitor.sh
+++ monitor.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+LOGFILE="health_report.log"
 
 echo "SERVER HEALTH REPORT"
 echo "Generated on: $(date)"
@@ -24,3 +25,39 @@ echo ""
 echo "Disk Usage:"
 
 df -h
+
+
+echo ""
+echo "Nginx Status:"
+
+
+if systemctl is-active --quiet nginx
+then
+	echo "Nginx is Running"
+else
+	echo "Nginx is Stopped"
+
+fi
+
+
+echo ""
+
+
+
+DISK=$(df / | awk 'NR==2 {print $5}' | sed 's/%//')
+
+if [ "$DISK" -gt 80 ]
+then
+	echo "WAENING: Disk usage above 80%"
+fi
+
+
+MEM=$(free | awk '/Mem:/ {printf("%.0f"), $3/$2*100}')
+
+if [ "$MEM" -gt 80 ]
+then
+	echo "Warning: High Memory Usage"
+fi
+
+
+
